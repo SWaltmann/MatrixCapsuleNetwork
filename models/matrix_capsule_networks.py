ed584cb6e33640816fda4621d89b7c4352cb0ad6
Stijn Waltmann <swaltmann98@gmail.com>
2025-06-30 00:12:09 +0200

    Rename and clean-up model architecture file
    Remove commented out code and rename it to matrix_capsule_networks, which is what are defined there. Previous name was using naming convention of previous repo, which no longer made sense

--- models/matrix_capsule_networks.py
+++ models/matrix_capsule_networks.py
@@ -1,11 +1,9 @@
 
 import numpy as np
-if __name__ != '__main__':
-    # Didnt need these for some small tests (okay really ugly I know will remove this some time)
-    import tensorflow as tf
-    from utils.layers_em_hinton import ReLUConv, PrimaryCaps, ConvCaps, ClassCaps, EMRouting, DebugLayer, Squeeze
+import tensorflow as tf
+from utils.layers_em_hinton import ReLUConv, PrimaryCaps, ConvCaps, ClassCaps, EMRouting, DebugLayer, Squeeze
 
-def em_capsnet_graph(input_shape, mode):
+def em_capsnet_graph(input_shape):
     """ Architecture of EM CapsNet, as described in: 'Matrix Capsules with EM Routing '
 
     Each layer is named after what their output represents
@@ -15,15 +13,9 @@ def em_capsnet_graph(input_shape, mode):
     height, width = input_shape[0], input_shape[1]
     x = np.linspace(-1, 1, height)
     y = np.linspace(-1, 1, width)
-
     position_grid = np.meshgrid(x, y)
 
-
     inputs = tf.keras.Input(input_shape)
-    # Other models in this repo need the y_true tensor for the reconstruction 
-    # regularizer. We do not use y_true, but accept the input so that we can 
-    # use the same Dataset object for training/testing
-    y_true = tf.keras.layers.Input(shape=(5,))
 
     relu_conv1 = ReLUConv(A=32)(inputs)
     position_grid = position_grid_conv(position_grid, 5, 2, 'VALID') 
@@ -45,16 +37,9 @@ def em_capsnet_graph(input_shape, mode):
 
     poses, acts = outputs
 
-    # TODO: if there are two inputs, return this model (commented out)
-    # Makes sure we can use the same dataset as the other models, maybe better
-    # to just add an error message or something
-    # return tf.keras.Model(inputs=[inputs, y_true],outputs=prim_caps1, name='EM_CapsNet')
+    return tf.keras.Model(inputs=inputs,outputs=acts, name='full_EM_CapsNet')
 
-    return tf.keras.Model(inputs=inputs,outputs=outputs, name='EM_CapsNet')
-
-def small_em_capsnet_graph(input_shape, mode, debug=True):
-    # Create position grid
-    # input shape = [48, 48, 2]
+def small_em_capsnet_graph(input_shape):
     height, width = input_shape[0], input_shape[1]
     x = np.linspace(-1, 1, height)
     y = np.linspace(-1, 1, width)
@@ -63,38 +48,23 @@ def small_em_capsnet_graph(input_shape, mode, debug=True):
 
 
     inputs = tf.keras.Input(input_shape)
-    # Other models in this repo need the y_true tensor for the reconstruction 
-    # regularizer. We do not use y_true, but accept the input so that we can 
-    # use the same Dataset object for training/testing
-    y_true = tf.keras.layers.Input(shape=(5,))
 
     relu_conv1 = ReLUConv(A=64)(inputs)
-    # relu_conv1 = DebugLayer("relu_conv")(relu_conv1)
     position_grid = position_grid_conv(position_grid, 5, 2, 'VALID') 
 
     prim_caps1 = PrimaryCaps(B=8)(relu_conv1)
-    # prim_caps1 = DebugLayer("primcaps")(prim_caps1)
     position_grid = position_grid_conv(position_grid, 1, 1, 'SAME')
 
     conv_caps1 = ConvCaps(C=16, stride=2)(prim_caps1)
-    # conv_caps1 = DebugLayer("convcaps1")(conv_caps1)
     position_grid = position_grid_conv(position_grid, 3, 2, 'VALID')
     routing1 = EMRouting(name='routing1')(conv_caps1) 
-    # routing1 = DebugLayer("routing1")(routing1) 
   
     conv_caps2 = ConvCaps(C=16)(routing1)
-    # conv_caps2 = DebugLayer("convcaps2")(conv_caps2)
     position_grid = position_grid_conv(position_grid, 3, 1, 'VALID')
     routing2 = EMRouting()(conv_caps2) 
-    # routing2 = DebugLayer("routing2")(routing2) 
 
-
-    # class_caps = ClassCaps(position_grid)(routing2)
-    class_caps = ConvCaps(C=5, kernel_size=8)(routing2)
-    # class_caps = DebugLayer("classcaps")(class_caps)
-
+    class_caps = ClassCaps(position_grid)(routing2)
     outputs = EMRouting()(class_caps) 
-    # outputs = DebugLayer("routing_classcaps")(outputs)
 
     outputs = Squeeze()(outputs)
 
@@ -107,9 +77,6 @@ def position_grid(grid, kernel_size, stride, padding):
     # last dimension is the x and y coordinates
     # Kernel should thus look like (kernel, kernel, 2, 2)
     #   So two outputs: an x and a y
-
-    # TODO: Make a manual function that does this better. This gives issues 
-    # due to padding, and really it is not that hard to calculate this myself
     x_kernel = tf.stack([tf.ones((kernel_size, kernel_size), dtype=tf.float32),
                          tf.zeros((kernel_size, kernel_size), dtype=tf.float32)],
                         axis=-1)
